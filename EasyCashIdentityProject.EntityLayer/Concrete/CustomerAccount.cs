c2bbb687402950b6507bb2c57b5865cce10b54b1
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-09-30 13:30:11 +0300

    IdentityDbContext added

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccount
+    {
+
+        public int CustomerAccountID { get; set; }
+
+        public string AccountNumber { get; set; }
+        public string Currency { get; set; }
+        public decimal Balance { get; set; }
+
+        public string BankBranch { get; set; }
+    }
+}
